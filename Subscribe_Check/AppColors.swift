a48d8273bfa15b7e35f896f06bd0e8ee9b4a06cf
yeonwoo <yeonwoo@yeonwooui-Mac.local>
2026-05-17 22:04:36 +0900

    feat: AppColors 디자인 시스템 추가

---
+++ Subscribe_Check/AppColors.swift
@@ -0,0 +1,19 @@
+//
+//  AppColors.swift
+//  Subscribe_Check
+//
+//  Created by yeonwoo on 2026/05/17.
+//
+
+import UIKit
+
+struct AppColors {
+    static let primary = UIColor(red: 74/255, green: 144/255, blue: 217/255, alpha: 1.0)
+    static let positive = UIColor(red: 52/255, green: 199/255, blue: 89/255, alpha: 1.0)
+    static let warning = UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1.0)
+    static let danger = UIColor(red: 255/255, green: 59/255, blue: 48/255, alpha: 1.0)
+    static let background = UIColor(red: 242/255, green: 242/255, blue: 247/255, alpha: 1.0)
+    static let cardBackground = UIColor.white
+    static let textPrimary = UIColor(red: 28/255, green: 28/255, blue: 30/255, alpha: 1.0)
+    static let textSecondary = UIColor(red: 142/255, green: 142/255, blue: 147/255, alpha: 1.0)
+}
